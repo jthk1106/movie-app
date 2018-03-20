4b28ae905dfb9bdaf0997c23166dda58a5f95802
Jeremy Kim <thk116@gmail.com>
2018-03-20 01:13:57 +0000

    updated search component ts and html with displayMovie branch

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -10,16 +10,33 @@ import { MovieService } from '../movie.service';
 export class SearchComponent implements OnInit {
 
   constructor(private _movie: MovieService) { }
-    
+  response: any;
+  
+  
   ngOnInit() {
   }
   
-  newsearch(){
-    this._movie.getData()
+  newsearch(search){
+    this._movie.getData(search)
       .subscribe( data => {
         //this.data=data.results
         console.log(data)
+        this.response = data.results
+        console.log(this.response)
       })
+  }
+  
+  savemovie(){
+    
   }
 
 }
+/*searchMovie(movie){
+    this._movie.getData(movie)
+      .subscribe(response => {
+        console.log(response)
+        this.data = response.results
+        console.log(this.data)
+      }
+      )
+  }*/
