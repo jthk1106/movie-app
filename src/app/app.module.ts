650f2791b6fcd55ca286eb79c010350e01b4655e
Jeremy Kim <thk116@gmail.com>
2019-06-02 02:53:16 -0700

    added back postinstall script

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 import { ClarityModule } from '@clr/angular';
 import { AppComponent } from './app.component';
 import { ROUTING } from "./app.routing";
@@ -33,7 +32,6 @@ import { SaveService } from './save.service';
         BrowserAnimationsModule,
         BrowserModule,
         FormsModule,
-        HttpModule,
         ClarityModule,
         ROUTING,
         HttpClientModule
