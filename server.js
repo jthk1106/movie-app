fa9b1371ebe658eb6e997974a04ce0416d277341
Jeremy Kim <thk116@gmail.com>
2019-06-04 00:11:37 -0700

    edited server.js app.use path

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/movie-app-pro>'));
+app.use(express.static(__dirname + '/dist>'));
+//app.use(express.static(__dirname + '/dist/movie-app-pro>'));
 
 app.get('/*', function(req,res) {
     const index = path.join(__dirname, 'dist', 'index.html');
